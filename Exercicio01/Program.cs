02bdc2686769194a0ca9f580b95a6c763bc6187f
Rodrigo de Araujo <rodrigoaraujo1709@gmail.com>
2021-10-15 19:26:40 -0300

    Exercicio01 feito

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma 
+            mensagem explicativa, conforme exemplos.
+             */
+            Console.WriteLine("Digite dois valores inteiros para somar: ");
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int Soma = A + B;
+            Console.WriteLine("SOMA = " + Soma);
+        }
+    }
+}
