eb1d3a296e39e3c8b45dffc6f109c770e971821d
Rodrigo de Araujo <rodrigoaraujo1709@gmail.com>
2021-10-15 19:35:44 -0300

    Exercicio03 feito

---
+++ Exercicio03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto 
+            de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D)
+             */
+            int A, B, C, D, dif;
+            Console.WriteLine("Digite o primeiro numero: ");
+            A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero: ");
+            B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o terceiro numero: ");
+            C = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o quarto numero: ");
+            D = int.Parse(Console.ReadLine());
+            dif = A * B - C * D;
+            Console.WriteLine("DIFERENCA = " + dif);
+        }
+    }
+}
