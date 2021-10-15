357e2d1b5f67f9ac33030d5d5b1b08780b370734
Rodrigo de Araujo <rodrigoaraujo1709@gmail.com>
2021-10-15 19:30:31 -0300

    Exercicio02 feito

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro 
+            casas decimais conforme exemplos.
+             */
+            double Raio, Area;
+            const double pi = 3.14159;
+
+            Raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Area = pi * Math.Pow(Raio,2);
+
+            Console.WriteLine("A=" + Area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
