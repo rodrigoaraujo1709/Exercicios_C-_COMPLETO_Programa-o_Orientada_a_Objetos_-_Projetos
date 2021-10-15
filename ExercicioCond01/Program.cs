82b4deeb8e32bb2eabcc433cf922263b53c30033
Rodrigo de Araujo <rodrigoaraujo1709@gmail.com>
2021-10-15 20:16:04 -0300

    ExercicioCond01 e ExercicioCond02 feitos

---
+++ ExercicioCond01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExercicioCond01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não. 
+             */
+            
+            Console.Write("Digite um numero inteiro para saber se é positivo ou negativo: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n > 0)
+            {
+                Console.WriteLine("NÃO NEGATIVO");
+            }
+            else
+            {
+                Console.WriteLine("NEGATIVO");
+            }
+        }
+    }
+}
---
+++ ExercicioCond02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExercicioCond02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero inteiro para saber se é par ou impar: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+        }
+    }
+}
