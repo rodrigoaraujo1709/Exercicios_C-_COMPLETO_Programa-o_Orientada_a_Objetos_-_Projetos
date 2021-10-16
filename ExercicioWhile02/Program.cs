125938ce5891cdcbd17b53ace74e742f47b91f5f
Rodrigo de Araujo <rodrigoaraujo1709@gmail.com>
2021-10-15 21:09:23 -0300

    ExercicioWhile02

---
+++ ExercicioWhile02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ExercicioWhile02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema 
+            cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo 
+            menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma). 
+             */
+            Console.WriteLine("Digite as duas coordenadas: ");
+            string[] valores = Console.ReadLine().Split(' ');
+            int x = int.Parse(valores[0]);
+            int y = int.Parse(valores[1]);
+
+            while (x != 0 && y != 0)
+            {
+                if (x > 0 && y > 0)
+                {
+                    Console.WriteLine("primeiro");
+                }
+                else if (x < 0 && y > 0)
+                {
+                    Console.WriteLine("segundo");
+                }
+                else if (x < 0 && y < 0)
+                {
+                    Console.WriteLine("terceiro");
+                }
+                else
+                {
+                    Console.WriteLine("quarto");
+                }
+                valores = Console.ReadLine().Split(' ');
+                x = int.Parse(valores[0]);
+                y = int.Parse(valores[1]);
+            }
+        }
+    }
+}
